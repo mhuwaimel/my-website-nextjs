383806ef4c10f61e01a467411975c51406960854
mhuwaimel <alhuwaimel.m@outlook.sa>
2024-12-02 11:42:07 +0300

    update-my-experience

---
+++ data/experience.jsx
@@ -0,0 +1,101 @@
+const resumData = [
+  {
+    Id: 1,
+    position: "مدير قسم أداء التعليم",
+    Place: "",
+    Description:
+      "مكتب التعليم بمحافظة الرين –  إدارة التعليم بمحافظة القويعية.",
+    Start: "2024",
+    EndJob: "حالياً",
+    location: null,
+    image: null,
+  },
+  {
+    Id: 2,
+    position: "مساعد مدير المكتب للشؤون التعليمية",
+    Place: "",
+    Description:
+      "مكتب التعليم بمحافظة الرين –  إدارة التعليم بمحافظة القويعية.",
+    Start: "2019",
+    EndJob: "2024",
+    location: null,
+    image: null,
+  },
+
+  {
+    Id: 3,
+    position: "عضو لجنة التنمية المحلية ",
+    Place: "",
+    Description:
+      "عضو لجنة التنمية المحلية بمحافظة الرين – وزارة الموارد البشرية والتنمية الاجتماعية ",
+    Start: "2018",
+    EndJob: "2020",
+    location: null,
+    image: null,
+  },
+  {
+    Id: 4,
+    position: "مشرف تربوي ",
+    Place: "",
+    Description:
+      "مشرف تربوي لمادة الحاسب الآلي بمكتب التعليم بمحافظة الرين - إدارة التعليم بمحافظة القويعية",
+    Start: "2017",
+    EndJob: "2019",
+    location: null,
+    image: null,
+  },
+  {
+    Id: 5,
+    position: "معلم حاسب آلي",
+    Place: "",
+    Description: "وزارة التعليم – إدارة التعليم بمحافظة القويعية ",
+    Start: "2011",
+    EndJob: "2017",
+    location: null,
+    image: null,
+  },
+  {
+    Id: 6,
+    position: "مبرمج حاسب آلي ",
+    Place: "شركة السلام للطائرات",
+    Description:
+      "شركة متعاقدة مع القوات الجوية الملكية السعودية – مشروع برنامج المساندة الفنية لطائرات F-15.",
+    Start: "2010",
+    EndJob: "2011",
+    location: null,
+    image: null,
+  },
+  {
+    Id: 7,
+    position: " مهندس حاسب آلي ",
+    Place: "شركة الإلكترونيات المتقدمة SAMI ",
+    Description:
+      "شركة متعاقدة مع القوات الجوية الملكية السعودية – مشروع برنامج المساندة الفنية لطائرات F-15.",
+    Start: "2008",
+    EndJob: "2010",
+    location: null,
+    image: null,
+  },
+  {
+    Id: 8,
+    position: "اخصائي نظم معلومات ",
+    Place: "القوات الجوية الملكية السعودية ",
+    Description: "القوات الجوية الملكية السعودية –هيئة إدارة القوات الجوية ",
+    Start: "2007",
+    EndJob: "2008",
+    location: null,
+    image: null,
+  },
+  {
+    Id: 9,
+    position: "اخصائي تقنية معلومات",
+    Place: "الشركة الوطنية لصناعة البطاريات NBC ",
+    Description:
+      "الشركة الوطنية لصناعة البطاريات NBC  احدى شركات شركة التصنيع الوطنية ",
+    Start: "2006",
+    EndJob: "2007",
+    location: null,
+    image: null,
+  },
+];
+export default resumData;
