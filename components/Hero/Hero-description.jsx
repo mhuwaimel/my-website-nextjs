c18bb72d763f3dbb80d3ddfbe810c68f905ddc1d
mhuwaimel <alhuwaimel.m@outlook.sa>
2025-01-22 16:29:34 +0300

    update-mainPage

---
+++ components/Hero/Hero-description.jsx
@@ -0,0 +1,20 @@
+"use client";
+import { motion } from "framer-motion";
+import { textVariant } from "@/utils/animation";
+import TextWriter from "../TextWriter";
+function HeroDescription({ heroDescriptions }) {
+  return (
+    <motion.p
+      variants={textVariant(1.3)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true }}
+      className="max-w-xl py-5 mx-auto leading-8 tracking-normal font-[Lateef]  
+              text-3xl text-slate-600  md:text-4xl"
+    >
+      <TextWriter text={heroDescriptions} delay={0.037} />
+    </motion.p>
+  );
+}
+
+export default HeroDescription;
--- components/Hero/HeroText.jsx
+++ components/Hero/HeroText.jsx
@@ -4,6 +4,7 @@ import { staggerContainer, textVariant } from "@/utils/animation";
 
 import AnimatedDownloadButton from "./AnimatedDownloadButton";
 import TextWriter from "../TextWriter";
+import HeroDescription from "./Hero-description";
 
 export default function HeroText({
   heroTitleName,
@@ -31,16 +32,7 @@ export default function HeroText({
       <h3 className="py-2 text-lg tracking-tight md:text-3xl font-[cairo]">
         <TextWriter text={heroDesignation} delay={0.1} />
       </h3>
-      <motion.p
-        variants={textVariant(1.3)}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: true }}
-        className="max-w-xl py-5 mx-auto leading-8 tracking-normal font-[Lateef]  
-              text-3xl text-slate-600  md:text-4xl"
-      >
-        <TextWriter text={heroDescriptions} delay={0.05} />
-      </motion.p>
+      <HeroDescription heroDescriptions={heroDescriptions} />
       <div className="text-right  md:text-center">
         <AnimatedDownloadButton />
       </div>
